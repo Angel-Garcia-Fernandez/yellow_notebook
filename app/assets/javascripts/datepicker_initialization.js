aaea0cdbf017fe3bc49a736a9302bd8bb2a60d58
angel <angel.garciafernandez@gmail.com>
2016-10-08 09:57:46 +0100

    dropdown not working in production

--- app/assets/javascripts/datepicker_initialization.js
+++ app/assets/javascripts/datepicker_initialization.js
@@ -1,11 +1,11 @@
-/*function datepicker_observer() {
+function datepicker_observer() {
     $('.datetimepicker').datetimepicker( { locale: 'es' } );
     //$('.datepicker').datetimepicker( { locale: 'es', format: 'LL' } );
     $('.datepicker').datetimepicker( { locale: 'es', format: 'YYYY-M-D' } );
     $('.timepicker').datetimepicker( { locale: 'es', format: 'LT' } );
-};*/
+};/*
 function datepicker_observer() {
     $('.datetimepicker').datetimepicker();
     $('.datepicker').datetimepicker( { format: "dddd, MMMM Do YYYY" } );
     $('.timepicker').datetimepicker( { format: 'LT' } );
-};
+};*/
