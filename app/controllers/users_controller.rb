4d7bea038e2ed327b28221f89e69b21ee0242655
angel <angel.garciafernandez@gmail.com>
2016-10-17 13:42:36 +0100

    ordering bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all.order( :name )
+    @users = User.all.order( :login )
   end
 
   # GET /users/1
